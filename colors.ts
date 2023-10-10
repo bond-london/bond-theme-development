d082e18d1496409cdeef7ca498e56c72cf43aa54
nrandell <nick@nickrandell.com>
2023-10-10 13:28:17 +0100

    Updated eslint rules

--- colors.ts
+++ colors.ts
@@ -9,6 +9,9 @@ export function lookupColourString(colour: ColourName, type:ColourType): string 
   }
   throw new Error("Unsupported colour type " + type);
 }  
+
+export type GradientName = "Transparent";
+export const lookupGradientOrColourString = lookupColourString;
 export const colourOpposites= {  "Black": "White",  "White": "Black",  "Grey": "Blue",  "Blue": "Grey",  "Red": "Green",  "Green": "Red",};
 export type OppositeName = keyof typeof colourOpposites;
 export function lookupOpposite(colour: ColourName): ColourName {
@@ -17,16 +20,16 @@ export function lookupOpposite(colour: ColourName): ColourName {
   return entry as ColourName;      
 }
     
-export function lookupColourClassNames(backgroundColour?: ColourName | null, textColour?: ColourName | null): string|undefined {
+export function lookupColourClassNames(backgroundColour?: ColourName | GradientName | null, textColour?: ColourName | null): string|undefined {
   if (backgroundColour) {
-    const backgroundColourClassName = lookupColourString(backgroundColour, "bg");
+    const backgroundColourClassName = lookupGradientOrColourString(backgroundColour, "bg");
     if (textColour) {
       const textColourClassName = lookupColourString(textColour, "text");
       return classNames(backgroundColourClassName, textColourClassName);
     }
-      const opposite = lookupOpposite(backgroundColour as OppositeName);
-      const textColourClassName = lookupColourString(opposite, "text");
-      return classNames(backgroundColourClassName, textColourClassName);
+    const opposite = lookupOpposite(backgroundColour as OppositeName);
+    const textColourClassName = lookupColourString(opposite, "text");
+    return classNames(backgroundColourClassName, textColourClassName);
   }
   if (textColour) {
     const textColourClassName = lookupColourString(textColour, "text");
